3c30d7e56a1cae1de46cfd31be0db1b34aaf0695
Avi Krishnan <cosmiczilch@gmail.com>
2019-12-28 00:33:27 +0530

    Updated dockerrun.sh to accept Local|Test

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --restart always -d -e app_environment=Test -e app_name=bonda -e app_dir_path=/go/src/github.com/spacetimi/bonda_server --publish 8000:8000 bonda-server | xargs -I containerId docker logs -f containerId
+# Run the docker container for bonda against $1 environment
+if [ $1 = "Local" ] || [ $1 = "Test" ]; then
+   docker run --restart always -d -e app_environment=$1 -e app_name=bonda -e app_dir_path=/go/src/github.com/spacetimi/bonda_server --publish 8000:8000 bonda-server | xargs -I containerId docker logs -f containerId
+else
+   echo "Usage: dockerrun.sh <Local|Test>"
+fi
+
