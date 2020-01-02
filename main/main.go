af170e1bd24c8b891ab5a264a6951ab676f3a7ad
Avi Krishnan <cosmiczilch@gmail.com>
2020-01-03 00:48:33 +0530

    Split servers into api server and admin server

--- main/main.go
+++ main/main.go
@@ -4,14 +4,29 @@ import (
 	"github.com/spacetimi/bonda_server/app_src/app_init"
 	"github.com/spacetimi/bonda_server/dummy_controller"
 	"github.com/spacetimi/bonda_server/testing"
-	"github.com/spacetimi/timi_shared_server/code/core/shared_init"
+	"github.com/spacetimi/timi_shared_server/code/admin_server"
 	"github.com/spacetimi/timi_shared_server/code/api_server"
+	"github.com/spacetimi/timi_shared_server/code/core/shared_init"
+	"sync"
 )
 
 func main() {
 
 	shared_init.SharedInit(app_init.GetAppInitializer())
 
-	// TODO: Remove these
-	api_server.StartServer(testing.TestingController, dummy_controller.DummyController)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+	    defer wg.Done()
+		api_server.StartServer(testing.TestingController, dummy_controller.DummyController)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		admin_server.StartServer()
+	}()
+
+	wg.Wait()
 }
