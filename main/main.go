c77ef90e4ebf91eacc3edac55287c5d4431e2756
Avi Krishnan <cosmiczilch@gmail.com>
2020-01-04 02:56:05 +0530

    Move api server and admin server to same port (8000)

--- main/main.go
+++ main/main.go
@@ -4,29 +4,13 @@ import (
 	"github.com/spacetimi/bonda_server/app_src/app_init"
 	"github.com/spacetimi/bonda_server/dummy_controller"
 	"github.com/spacetimi/bonda_server/testing"
-	"github.com/spacetimi/timi_shared_server/code/admin_server"
-	"github.com/spacetimi/timi_shared_server/code/api_server"
 	"github.com/spacetimi/timi_shared_server/code/core/shared_init"
-	"sync"
+	"github.com/spacetimi/timi_shared_server/code/server"
 )
 
 func main() {
 
 	shared_init.SharedInit(app_init.GetAppInitializer())
 
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-	    defer wg.Done()
-		api_server.StartServer(testing.TestingController, dummy_controller.DummyController)
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		admin_server.StartServer()
-	}()
-
-	wg.Wait()
+    server.StartServer(testing.TestingController, dummy_controller.DummyController)
 }
