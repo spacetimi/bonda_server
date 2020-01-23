897d3b0cbf3f4b1457eb0a4a43f981d7c8027e4c
Avi Krishnan <cosmiczilch@gmail.com>
2020-01-23 22:20:07 +0530

    Use bind-mount to make aws creds accessible to docker container running locally

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Run the docker container for bonda against $1 environment
+# See also: dockerbuild.sh for building the docker image locally
 if [ $1 = "Local" ] || [ $1 = "Test" ]; then
-   docker run --restart always -d -e app_environment=$1 -e app_name=bonda -e app_dir_path=/go/src/github.com/spacetimi/bonda_server --publish 8000:8000 bonda-server | xargs -I containerId docker logs -f containerId
+   docker run --restart always -d -v $HOME/.aws:/root/.aws -e app_environment=$1 -e app_name=bonda -e app_dir_path=/go/src/github.com/spacetimi/bonda_server --publish 8000:8000 bonda-server | xargs -I containerId docker logs -f containerId
 else
    echo "Usage: dockerrun.sh <Local|Test>"
 fi
