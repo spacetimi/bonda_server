830306f00c15668655d1a5440e10fa60939ba821
Avi Krishnan <cosmiczilch@gmail.com>
2020-01-28 21:38:01 +0530

    Register metadata factories' regiatration in app_init

--- app_src/app_init/app_init.go
+++ app_src/app_init/app_init.go
@@ -1,6 +1,10 @@
 package app_init
 
-import "github.com/spacetimi/timi_shared_server/code/core/shared_init"
+import (
+	"github.com/spacetimi/timi_shared_server/code/core/services/metadata_service/metadata_factory"
+	"github.com/spacetimi/timi_shared_server/code/core/services/metadata_service/metadata_typedefs"
+	"github.com/spacetimi/timi_shared_server/code/core/shared_init"
+)
 
 func GetAppInitializer() shared_init.IAppInitializer {
 	return &appInitializer
@@ -12,8 +16,31 @@ var appInitializer AppInitializer
 
 /********** Begin IAppInitializer implementation **********/
 func (appInitializer *AppInitializer) AppInit() {
-	// TODO: Nothing to do yet
+	registerMetadataFactories()
 }
 /********** End IAppInitializer implementation **********/
+
+// TODO: Avi: Move this somewhere else?
+func registerMetadataFactories() {
+	metadata_factory.RegisterFactory("MetadataTest", MetadataTestFactory{})
+}
+
+type MetadataTestFactory struct {	// Implements IMetadataFactory
+}
+
+func (f MetadataTestFactory) Instantiate() metadata_typedefs.IMetadataItem {
+	return &MetadataTest{}
+}
+
+// TODO: Avi: Move this somewhere else
+type MetadataTest struct {
+	Id int
+}
+func (m MetadataTest) GetKey() string {
+	return "MetadataTest"
+}
+func (m MetadataTest) GetMetadataSpace() metadata_typedefs.MetadataSpace {
+	return metadata_typedefs.METADATA_SPACE_APP
+}
 
 
